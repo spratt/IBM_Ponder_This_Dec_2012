bf0baea30e9673278db38ad0c11b192af7da628d
Simon Pratt <spratt@scs.carleton.ca>
2012-12-06 20:36:33 -0500

    Now creating all_ones and checkerboard

--- grid.py
+++ grid.py
@@ -1,30 +1,83 @@
 #!/usr/bin/env python2.6
 
-ROWS = 6
-COLS = 6
+class Grid(object):
+    def __init__(self,rows,cols):
+        self.rows = rows
+        self.cols = cols
+        self._grid = [[1 for _ in range(cols)] for _ in range(rows)]
 
-grid = [[1 for _ in range(ROWS)] for _ in range(COLS)]
+    def __str__(self):
+        s = ""
+        for row in self._grid:
+            for col in row:
+                s += "{0} ".format(col)
+            s += "\n"
+        return s
 
-def printGrid(grid):
-    for col in grid:
-        for row in col:
-            print row,
-        print
+    def sum(self):
+        return sum([sum(col) for col in self._grid])
 
-    print "Sum:",sum([sum(col) for col in grid])
+    def isValidPos(self,row,col):
+        return (0 <= row < self.rows) and (0 <= col < self.cols)
+
+    def getNeighbours(self,row,col):
+        nborVals = set()
+        nborVals.add(self.getVal(row-1,col)) # up
+        nborVals.add(self.getVal(row+1,col)) # down
+        nborVals.add(self.getVal(row,col-1)) # left
+        nborVals.add(self.getVal(row,col+1)) # right
+        return nborVals
+
+    def isPosValid(self,row,col):
+        if not self.isValidPos(row,col):
+            return False
+        return self.getVal(row,col) == self.maxVal(row,col)
+
+    def getVal(self,row,col):
+        if self.isValidPos(row,col):
+            return self._grid[row][col]
+        return 0
+
+    def setVal(self,row,col,val):
+        if self.isValidPos(row,col):
+            self._grid[row][col] = val
+        self.makeValid()
+
+    def maxVal(self,row,col):
+        nborVals = self.getNeighbours(row,col)
+        for i in range(len(nborVals)+1):
+            if (i+1) not in nborVals:
+                return (i+1)
+
+    def isValid(self):
+        valid = True
+        for row in range(self.rows):
+            for col in range(self.cols):
+                valid = valid and self.isPosValid(row,col)
+        return valid
+
+    def makeValid(self):
+        for row in range(self.rows):
+            for col in range(self.cols):
+                if not self.isPosValid(row,col):
+                    self.setVal(row,col,self.maxVal(row,col))
 
 def isEven(n):
     return 0 == (n%2)
 
-print "All ones"
-printGrid(grid)
+if __name__ == '__main__':
+    g = Grid(6,6)
 
-for col in range(len(grid)):
-    for row in range(len(grid[col])):
-        if isEven(col) and isEven(row):
-            grid[col][row] = 2
-        elif not isEven(col) and not isEven(row):
-            grid[col][row] = 2
+    print "All ones"
+    print g
+    print "Sum:",g.sum()
 
-print "Checkerboard"
-printGrid(grid)
+    print "Checkerboard"
+    g.makeValid()
+    print g
+    print "Sum:",g.sum()
+
+    print "makeValid(Checkerboard)"
+    g.makeValid()
+    print g
+    print "Sum:",g.sum()
---
+++ gridTests.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python2.6
+
+from grid import Grid
+
+print "----------------------------------------------------------------------"
+print "| Grid(6,4)                                                          |"
+print "----------------------------------------------------------------------"
+
+ROWS = 6
+COLS = 4
+
+g = Grid(ROWS,COLS)
+
+y,x = 0,0
+print "({0},{1}) is valid? {2}".format(y,x,g.isValidPos(y,x))
+y,x = ROWS-1,0
+print "({0},{1}) is valid? {2}".format(y,x,g.isValidPos(y,x))
+y,x = 0,COLS-1
+print "({0},{1}) is valid? {2}".format(y,x,g.isValidPos(y,x))
+y,x = ROWS-1,COLS-1
+print "({0},{1}) is valid? {2}".format(y,x,g.isValidPos(y,x))
+y,x = -1,0
+print "({0},{1}) is valid? {2}".format(y,x,g.isValidPos(y,x))
+y,x = 0,-1
+print "({0},{1}) is valid? {2}".format(y,x,g.isValidPos(y,x))
+y,x = -1,-1
+print "({0},{1}) is valid? {2}".format(y,x,g.isValidPos(y,x))
+y,x = ROWS,0
+print "({0},{1}) is valid? {2}".format(y,x,g.isValidPos(y,x))
+y,x = 0,COLS
+print "({0},{1}) is valid? {2}".format(y,x,g.isValidPos(y,x))
+y,x = ROWS,COLS
+print "({0},{1}) is valid? {2}".format(y,x,g.isValidPos(y,x))
+
+print "----------------------------------------------------------------------"
+print "| Grid(6,6)                                                          |"
+print "----------------------------------------------------------------------"
+
+ROWS = 6
+COLS = 6
+
+g = Grid(ROWS,COLS)
+
+y,x = 0,0
+print "({0},{1}) is valid? {2}".format(y,x,g.isValidPos(y,x))
+y,x = ROWS-1,0
+print "({0},{1}) is valid? {2}".format(y,x,g.isValidPos(y,x))
+y,x = 0,COLS-1
+print "({0},{1}) is valid? {2}".format(y,x,g.isValidPos(y,x))
+y,x = ROWS-1,COLS-1
+print "({0},{1}) is valid? {2}".format(y,x,g.isValidPos(y,x))
+y,x = -1,0
+print "({0},{1}) is valid? {2}".format(y,x,g.isValidPos(y,x))
+y,x = 0,-1
+print "({0},{1}) is valid? {2}".format(y,x,g.isValidPos(y,x))
+y,x = -1,-1
+print "({0},{1}) is valid? {2}".format(y,x,g.isValidPos(y,x))
+y,x = ROWS,0
+print "({0},{1}) is valid? {2}".format(y,x,g.isValidPos(y,x))
+y,x = 0,COLS
+print "({0},{1}) is valid? {2}".format(y,x,g.isValidPos(y,x))
+y,x = ROWS,COLS
+print "({0},{1}) is valid? {2}".format(y,x,g.isValidPos(y,x))
